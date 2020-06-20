97f2d6e747bc94580a7288c9bb0249033e642d63
João <joaomljcanal@gmail.com>
2020-06-20 11:15:18 -0300

    Add Fake API and Product list component

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,9 +1,38 @@
 <template>
-  <div></div>
+  <section class="products-container">
+    <div v-for="product in products" :key="product.id">
+      <img
+        v-if="product.photos"
+        :src="product.photos[0].src"
+        :alt="product.fotos[0].title"
+      />
+      <p class="price">{{ product.price }}</p>
+      <h2 class="title">{{ product.name }}</h2>
+      <p>{{ product.description }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      products: null,
+    };
+  },
+  methods: {
+    getProducts() {
+      axios.get('http://localhost:3000/product').then((response) => {
+        this.products = response.data;
+      });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
 </script>
 
 <style></style>
