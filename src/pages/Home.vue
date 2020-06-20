659125fa452bcc5eb24d85376cdf1ae50c1f3865
João <joaomljcanal@gmail.com>
2020-06-20 10:31:26 -0300

    Add Home Page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div></div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <SearchProducts />
+    <ProductList />
+  </section>
 </template>
 
 <script>
+import SearchProducts from '@/components/SearchProducts';
+import ProductList from '@/components/ProductList';
+
 export default {
   name: 'Home',
+  components: {
+    SearchProducts,
+    ProductList,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
