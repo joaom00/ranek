bff7a42a15804096c861a1152fc9566b0346d546
João <joaomljcanal@gmail.com>
2020-06-20 10:07:31 -0300

    Add Header and Footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,87 @@
 <template>
   <div id="app">
-    <router-view />
+    <TheHeader />
+    <main>
+      <router-view />
+    </main>
+    <TheFooter />
   </div>
 </template>
 
-<style></style>
+<script>
+import TheHeader from '@/components/TheHeader';
+import TheFooter from '@/components/TheFooter';
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: Roboto, sans-serif;
+  color: #345;
+  background: url('./assets/pattern.svg') repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: Roboto, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+}
+</style>
