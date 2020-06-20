eab27313169c1d16103f9a1293a2a77a01252b11
João <joaomljcanal@gmail.com>
2020-06-20 19:13:44 -0300

    Add Product Page and Product Filter

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../pages/Home.vue';
+import Product from '../pages/Product.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/produto/:id',
+    name: 'Product',
+    component: Product,
+    props: true,
   },
 ];
 
