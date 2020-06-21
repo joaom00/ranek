eeaafe157da0f00a21a950f744d4b6a7ef339083
João <joaomljcanal@gmail.com>
2020-06-21 18:44:59 -0300

    Add Edit User

--- src/pages/User/UserEdit.vue
+++ src/pages/User/UserEdit.vue
@@ -1,9 +1,35 @@
 <template>
-  <p>Usuário Editar</p>
+  <section>
+    <UserForm>
+      <button class="btn" @click.prevent="updateUser">Atualizar Usuário</button>
+    </UserForm>
+  </section>
 </template>
 
 <script>
-export default {};
+import UserForm from '@/components/UserForm';
+import { api } from '@/services.js';
+
+export default {
+  name: 'UserEdit',
+  components: {
+    UserForm,
+  },
+
+  methods: {
+    updateUser() {
+      api
+        .put(`/user/${this.$store.state.user.id}`, this.$store.state.user)
+        .then(() => {
+          this.$store.dispatch('getUser');
+          this.$router.push({ name: 'User' });
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
