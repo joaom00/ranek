0018e7a972c54a4ce10d11a647c994cad0ddcf60
João <joaomljcanal@gmail.com>
2020-06-21 21:41:51 -0300

    Add User Purchases and Sales

--- src/pages/User/UserPurchase.vue
+++ src/pages/User/UserPurchase.vue
@@ -1,9 +1,73 @@
 <template>
-  <p>Compras do Usuário</p>
+  <section>
+    <div v-if="purchases">
+      <h2>Compras</h2>
+      <div
+        class="products-wrapper"
+        v-for="(purchase, index) in purchases"
+        :key="index"
+      >
+        <ProductItem v-if="purchase.product" :product="purchase.product">
+          <p class="seller"><span>Vendedor: </span>{{ purchase.seller_id }}</p>
+        </ProductItem>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProductItem from '@/components/ProductItem';
+import { api } from '@/services.js';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'UserPurchase',
+  components: {
+    ProductItem,
+  },
+
+  data() {
+    return {
+      purchases: null,
+    };
+  },
+
+  computed: {
+    ...mapState(['user', 'login']),
+  },
+
+  methods: {
+    getPurchases() {
+      api.get(`/transaction?buyer_id=${this.user.id}`).then((response) => {
+        this.purchases = response.data;
+      });
+    },
+  },
+
+  watch: {
+    login() {
+      this.getPurchases();
+    },
+  },
+
+  created() {
+    if (this.login) {
+      this.getPurchases();
+    }
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.product-wrapper {
+  margin-bottom: 40px;
+}
+
+.seller span {
+  color: #e80;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+</style>
--- src/pages/User/UserSale.vue
+++ src/pages/User/UserSale.vue
@@ -1,9 +1,96 @@
 <template>
-  <p>Vendas do Usuário</p>
+  <section>
+    <div v-if="sales">
+      <h2>Compras</h2>
+      <div class="products-wrapper" v-for="(sale, index) in sales" :key="index">
+        <ProductItem v-if="sale.product" :product="sale.product">
+          <p class="seller"><span>Comprador: </span>{{ sale.buyer_id }}</p>
+        </ProductItem>
+        <div class="delivery">
+          <h3>Entrega:</h3>
+          <ul>
+            <li>
+              <p>CEP: {{ sale.address.cep }}</p>
+              <p>Rua: {{ sale.address.street }}</p>
+              <p>Número: {{ sale.address.number }}</p>
+              <p>Bairro: {{ sale.address.neighborhood }}</p>
+              <p>Cidade: {{ sale.address.city }}</p>
+              <p>Estado: {{ sale.address.uf }}</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProductItem from '@/components/ProductItem';
+import { api } from '@/services.js';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'UserPurchase',
+  components: {
+    ProductItem,
+  },
+
+  data() {
+    return {
+      sales: null,
+      address: null,
+    };
+  },
+
+  computed: {
+    ...mapState(['user', 'login']),
+  },
+
+  methods: {
+    getSales() {
+      api.get(`/transaction?seller_id=${this.user.id}`).then((response) => {
+        this.sales = response.data;
+        this.address = response.data.address;
+      });
+    },
+  },
+
+  watch: {
+    login() {
+      this.getSales();
+    },
+  },
+
+  created() {
+    if (this.login) {
+      this.getSales();
+    }
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.product-wrapper {
+  margin-bottom: 40px;
+}
+
+.seller span {
+  color: #e80;
+}
+
+.delivery {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  grid-gap: 20px;
+  margin-bottom: 60px;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+h3 {
+  margin: 0;
+  justify-self: end;
+}
+</style>
